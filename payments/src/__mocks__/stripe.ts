830c17f10e67d1edffcdd3e72d5509f79e08bc06
Stefan Nestorovic <stefpepp@gmail.com>
2022-11-10 13:06:23 +0100

    Fix payments test

--- payments/src/__mocks__/stripe.ts
+++ payments/src/__mocks__/stripe.ts
@@ -1,7 +1,22 @@
 import { randomBytes } from 'crypto';
 
+// TODO: Change this after creating stripe account
 export const fakeStripe = {
     charges: {
-        create: jest.fn().mockResolvedValue({ id: randomBytes(8).toString('hex') })
+        create: jest.fn().mockImplementation((chargeOptions) => {
+            const { source } = chargeOptions;
+            if (source === 'tok_visa' || source.startsWith('tok_')) {
+                return {
+                    id: randomBytes(8).toString('hex'),
+                    status: 'OK',
+                    errors: []
+                }
+            }
+            return {
+                id: '',
+                status: 'FAILED',
+                errors: [{ message: 'Bad token' }]
+            }
+        })
     }
 }
